185a092374f954f92b5441ba87c144a02366ffe5
Okinz <steven14812@gmail.com>
2021-02-07 16:55:29 -0500

    Added delete route method

--- app.js
+++ app.js
@@ -45,6 +45,17 @@ app.post("/articles", function(req, res){
   });
 });
 
+app.delete("/articles", function(req, res){
+  
+  Article.deleteMany({}, function(err){
+    if(!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
